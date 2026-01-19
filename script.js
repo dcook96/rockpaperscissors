9d3a4c8a0dca72188ad0eab5d6f807b5356a2ad4
Darren Cook <darrenjcook96@gmail.com>
2026-01-18 19:19:21 -0500

    getCompterChoice

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log ('test')
+function getComputerChoice () {
+    let choice;
+    switch (Math.floor(Math.random() * 3)){
+        case 0:
+            return('rock');
+        case 1:
+            return('paper');
+        case 2:
+            return('scissors');
+    }
+}
